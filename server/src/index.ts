0f5ef19ea5dbf4cbf455b98dc44cb5464ad734d8
Seongwoo Kang <you3667@vaultmicro.com>
2022-01-10 19:19:28 +0900

    55분

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,7 +1,9 @@
 import express from 'express';
 import { graphqlHTTP } from 'express-graphql';
+import { schema } from './Schema'
 import cors from 'cors';
 import { createConnection } from 'typeorm';
+import { Users} from './Entities/Users';
 
 const main = async () => {
     await createConnection({
@@ -11,16 +13,16 @@ const main = async () => {
         password: 'qkdgkahr7365',
         logging: true,
         synchronize: false,
-        entities: [],
+        entities: [Users],
     });
 
     const app = express();
     app.use(cors());
     app.use(express.json());
-    // app.use("/graphql", graphqlHTTP({
-    //     schema,
-    //     graphiql: true
-    // })); 
+    app.use("/graphql", graphqlHTTP({
+        schema,
+        graphiql: true
+    })); 
 
     app.listen(3001, () => {
         console.log("SERVER RUNNING ON PORT 3001");
